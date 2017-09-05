ca0d022caa2831669465141b9b8ca839c841ae4d
Nikesh001 <nikkataria74@gmail.com>
2017-09-05 09:33:27 -0700

    santoni: Add libbt-vendor in device-common.mk

--- device.mk
+++ device.mk
@@ -117,7 +117,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    libbt-vendor \
+		android.hardware.bluetooth@1.0-impl
 # Camera
 PRODUCT_PACKAGES += \
     libshim_camera \
