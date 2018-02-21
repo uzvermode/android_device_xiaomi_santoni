ffe395535a7ef6579062985740235cd0fe7a5203
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2018-02-21 16:31:22 +0000

    santoni: Compile HWUI for Better Performance

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,6 +171,9 @@ TARGET_INIT_VENDOR_LIB := libinit_santoni
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 TARGET_RECOVERY_DEVICE_MODULES := libinit_santoni
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
